64f84b3908cc367c6819770fccc6b412e0721d99
applejxd <myamazaki.phys@gmail.com>
2021-03-14 19:51:22 +0900

    update windows terminal settings

--- personal_deploy.ps1
+++ personal_deploy.ps1
@@ -8,6 +8,6 @@ $path = $wsh.SpecialFolders("MyDocuments") + "\WindowsPowerShell"
 Remove-Item $path\Microsoft.PowerShell_profile.ps1 
 cmd /c mklink $path\Microsoft.PowerShell_profile.ps1 $env:UserProfile\src\windows-setup\config\Microsoft.PowerShell_profile.ps1
 
-# # Windows Terminal
-# Remove-Item $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json 
-# cmd /c mklink $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json $env:UserProfile\src\windows-setup\config\settings.json
+# Windows Terminal
+Remove-Item $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json 
+cmd /c mklink $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json $env:UserProfile\src\windows-setup\config\settings.json
