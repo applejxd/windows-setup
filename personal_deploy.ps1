cae1a57a583fc04702eb6a26094da2eedc73afff
applejxd <myamazaki.phys@gmail.com>
2021-03-15 22:14:57 +0900

    fix missing commands

--- personal_deploy.ps1
+++ personal_deploy.ps1
@@ -1,6 +1,10 @@
 # 個人設定
 
+# 特殊フォルダのパス取得のために Shell オブジェクトの生成
+$wsh = New-Object -ComObject WScript.Shell
+
 # gitconfig
+Remove-Item $env:UserProfile\.gitconfig
 cmd /c mklink $env:UserProfile\.gitconfig $env:UserProfile\src\windows-setup\config\.gitconfig
 
 # PowerShell
