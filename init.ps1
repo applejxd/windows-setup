f9da88ae26257412b731e2ae105544398b038d09
りんごくん <9639592+applejxd@users.noreply.github.com>
2021-05-07 12:32:29 +0900

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -10,7 +10,6 @@ Install-Module posh-git -Scope CurrentUser -Force
 Install-Module oh-my-posh -Scope CurrentUser -Force
 
 # Chocolatey のインストール
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install -y Boxstarter
