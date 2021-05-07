5352ade6f00ef76f6816d1ef94906752c81c7413
りんごくん <9639592+applejxd@users.noreply.github.com>
2021-05-07 18:32:53 +0900

    Update sw_deploy.ps1

--- sw_deploy.ps1
+++ sw_deploy.ps1
@@ -65,32 +65,27 @@ function InstallRelease($repo_name, $file_name) {
   if (-not (Test-Path $install_dir\$file_name)){
     # GitHub Release API
     $uri = "https://api.github.com/repos/" + $repo_name + "/releases/latest"
-    # JSON 取得
+    # Read json
     $json = Invoke-WebRequest $uri | ConvertFrom-Json
-    # ダウンロード URL 抜き出し
+    # Get URL
     $url = $json.assets.browser_download_url
-    # ダウンロード
+    # Download
     Invoke-WebRequest $url -OutFile "$install_dir\$file_name"
   }
 }
 
-# 短縮 URL
 InstallRelease "Fuhrmann/keypirinha-url-shortener" "URLShortener.keypirinha-package"
-# システムコマンド
 InstallRelease "psistorm/keypirinha-systemcommands" "SystemCommands.keypirinha-package"
-# クリップボード履歴
+# Clipborad Manager
 InstallRelease "tuteken/Keypirinha-Plugin-Ditto" "Ditto.keypirinha-package"
-# カラーピッカー
 InstallRelease "clinden/keypirinha-colorpicker" "ColorPicker.keypirinha-package"
-# スニペット
 InstallRelease "dozius/keypirinha-snippets" "Snippets.keypirinha-package"
-# Windows デフォルトアプリ
+# Default Windows Apps
 InstallRelease "ueffel/Keypirinha-WindowsApps" "WindowsApps.keypirinha-package"
-# Windows Terminal プロファイル
+# Windows Terminal Profiles
 InstallRelease "fran-f/keypirinha-terminal-profiles" "Terminal-Profiles.keypirinha-package"
-# 検索エンジンの短縮名で簡易検索
+# Search by abbrev
 InstallRelease "bantya/Keypirinha-EasySearch" "EasySearch.keypirinha-package"
-# > に続いてコマンド実行
+# Execute commands from >
 InstallRelease "bantya/Keypirinha-Command" "Command.keypirinha-package"
-# Steam アプリ
 Invoke-WebRequest "https://github.com/EhsanKia/keypirinha-plugins/raw/master/keypirinha-steam/build/Steam.keypirinha-package" -OutFile "$install_dir/Steam.keypirinha-package"
