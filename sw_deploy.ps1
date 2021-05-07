eefe5381454987518ea8072465371b0ba8cfaa98
りんごくん <9639592+applejxd@users.noreply.github.com>
2021-05-07 19:08:58 +0900

    Update sw_deploy.ps1

--- sw_deploy.ps1
+++ sw_deploy.ps1
@@ -1,11 +1,10 @@
-# 特殊フォルダのパス取得のために Shell オブジェクトの生成
+# for special folders
 $wsh = New-Object -ComObject WScript.Shell
 
-####################
-# 設定ファイルリンク #
-####################
+#################
+# Symbolic Link #
+#################
 
-# リポジトリのクローン
 $install_path = "$env:UserProfile\src\windows-setup"
 if (-not (Test-Path $install_path)){
   git clone https://github.com/applejxd/windows-setup.git $install_path
@@ -27,39 +26,32 @@ cmd /c mklink /D $env:AppData\Keypirinha $env:UserProfile\src\windows-setup\tool
 cmd /c rmdir /s /q $env:AppData\Everything
 cmd /c mklink /D $env:AppData\Everything $env:UserProfile\src\windows-setup\tools\Everything
 
-####################
-# スタートアップ登録 #
-####################
+###########
+# Startup #
+###########
 
-# ショートカット名を定義
 $path = $wsh.SpecialFolders("Startup") + "\keyhac.lnk"
 
 if (-not (Test-Path $path)){
-  # ショートカットオブジェクトの生成
   $shortcut = $wsh.CreateShortcut($path)
-
-  # 作業ディレクトリのパスを設定
+  
   $shortcut.WorkingDirectory = "C:\ProgramData\chocolatey\bin"
-  # ショートカット先のパスを設定
   $shortcut.TargetPath = "C:\ProgramData\chocolatey\bin\keyhac.exe"
-  # アイコンのパスを設定
   $shortcut.IconLocation = "C:\ProgramData\chocolatey\bin\keyhac.exe"
 
-  # ショートカットの生成
   $shortcut.Save()
 }
 
-###################
-# Keypirinha 拡張 #
-###################
+##############
+# Keypirinha #
+##############
 
-# インストールディレクトリ
 $install_dir = "$env:UserProfile\src\windows-setup\tools\Keypirinha\InstalledPackages"
 if (-not (Test-Path $install_dir)){
   New-Item $install_dir -ItemType Directory
 }
 
-# 各 Keypirinha 拡張をダウンロードする関数
+# function for downloading Keypirinha extensions
 function InstallRelease($repo_name, $file_name) {
   $install_dir = "$env:UserProfile\src\windows-setup\tools\Keypirinha\InstalledPackages"
   if (-not (Test-Path $install_dir\$file_name)){
