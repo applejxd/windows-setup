a031e9a3d85a6a626a0479996eb44df67df8e67f
りんごくん <9639592+applejxd@users.noreply.github.com>
2021-05-07 18:02:53 +0900

    Update personal_deploy.ps1

--- personal_deploy.ps1
+++ personal_deploy.ps1
@@ -4,14 +4,20 @@
 $wsh = New-Object -ComObject WScript.Shell
 
 # gitconfig
-Remove-Item $env:UserProfile\.gitconfig
+if (Test-Path $env:UserProfile\.gitconfig) {
+  Remove-Item $env:UserProfile\.gitconfig
+}
 cmd /c mklink $env:UserProfile\.gitconfig $env:UserProfile\src\windows-setup\config\.gitconfig
 
 # PowerShell
 $path = $wsh.SpecialFolders("MyDocuments") + "\WindowsPowerShell"
-Remove-Item $path\Microsoft.PowerShell_profile.ps1 
+if (Test-Path $path\Microsoft.PowerShell_profile.ps1) {
+  Remove-Item $path\Microsoft.PowerShell_profile.ps1 
+}
 cmd /c mklink $path\Microsoft.PowerShell_profile.ps1 $env:UserProfile\src\windows-setup\config\Microsoft.PowerShell_profile.ps1
 
 # Windows Terminal
-Remove-Item $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json 
+if (Test-Path $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json) {
+  Remove-Item $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json 
+}
 cmd /c mklink $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json $env:UserProfile\src\windows-setup\config\settings.json
