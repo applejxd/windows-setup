2d657788a69f63b85dd0517d00ff103a317fe0b3
applejxd <9639592+applejxd@users.noreply.github.com>
2021-09-15 23:59:01 +0900

    add script

---
+++ bin/mklink.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:カレントディレクトリを設定する
+cd /d %~dp0
+
+:シンボリックリンクを生成するためのbatファイルを作成する
+echo cd /d %~dp0 > temp.bat
+echo mklink /d %~n1 %1 >>temp.bat
+
+:管理者権限作成したbatを実行する( mklinkの実行には管理者権限がいる )
+powershell start-process temp.bat -verb runas
+
+:作成したbatファイルを削除する
+timeout 1
+del temp.bat
