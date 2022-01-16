b2286de08f720bf9de1ab7cba10f7f42b8cb6b9a
applejxd <9639592+applejxd@users.noreply.github.com>
2022-01-16 17:28:20 +0900

    add wsl function and change configs

--- config/Microsoft.PowerShell_profile.ps1
+++ config/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,7 @@
+# エンコーディングを US-ASCII から変更
+# cf. https://dattesar.com/powershell-utf8/
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+
 # cf. https://secon.dev/entry/2020/08/17/070735/
 
 # テーマ
@@ -11,6 +15,12 @@ Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 # 標準だと Ctrl+d は DeleteCharOrExit のため、うっかり端末が終了することを防ぐ
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
 
 #########
 # alias #
@@ -56,8 +66,13 @@ Set-PSReadLineKeyHandler -Chord 'Ctrl+x,Ctrl+f' -ScriptBlock {
   Clear-Host
 }
 
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+function wbk {
+  # Where-Object で空行削除
+  $distro = wsl -l -q | Where-Object{$_ -ne ""} | fzf
+  # null 文字を削除
+  # cf. https://stackoverflow.com/questions/9863455/how-to-remove-null-char-0x00-from-object-within-powershell
+  $distro = $distro -replace "`0",""
+  $date = Get-Date -UFormat "%y.%m.%d"
+  # https://sevenb.jp/wordpress/ura/2016/06/01/powershell%E6%96%87%E5%AD%97%E5%88%97%E5%86%85%E3%81%AE%E5%A4%89%E6%95%B0%E5%B1%95%E9%96%8B%E3%81%A7%E5%A4%89%E6%95%B0%E5%90%8D%E3%82%92%E7%A2%BA%E5%AE%9A%E3%81%95%E3%81%9B%E3%82%8B/
+  wsl --export "${distro}" "${distro}_${date}.tar"
 }
