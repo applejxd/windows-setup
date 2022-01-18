84ec259c959f524598174ae97bb9da7460fdd308
りんごくん <applejxd@users.noreply.github.com>
2022-01-18 12:38:10 +0900

    update

--- personal_deploy.ps1
+++ personal_deploy.ps1
@@ -1,10 +1,4 @@
 # 個人設定
 
-# 特殊フォルダのパス取得のために Shell オブジェクトの生成
-$wsh = New-Object -ComObject WScript.Shell
-
-# gitconfig
-if (Test-Path $env:UserProfile\.gitconfig) {
-  Remove-Item $env:UserProfile\.gitconfig
-}
-cmd /c mklink $env:UserProfile\.gitconfig $env:UserProfile\src\windows-setup\config\.gitconfig
+git config --global user.name applejxd
+git config --global user.email 9639592+applejxd@users.noreply.github.com
