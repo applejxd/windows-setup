46adb5e9f5eb2113641c6006a9412bc96a30b2dd
applejxd <9639592+applejxd@users.noreply.github.com>
2022-01-18 22:40:35 +0900

    update

--- personal.ps1
+++ personal.ps1
@@ -1,3 +1,10 @@
+##########
+# Config #
+##########
+
+git config --global user.name applejxd
+git config --global user.email 9639592+applejxd@users.noreply.github.co
+
 ##############
 # Boxstarter #
 ##############
--- personal_deploy.ps1
+++
@@ -1,4 +0,0 @@
-# 個人設定
-
-git config --global user.name applejxd
-git config --global user.email 9639592+applejxd@users.noreply.github.com
