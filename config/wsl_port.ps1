180290b495d8b5773000ccd9118fc0838fb75fab
applejxd <9639592+applejxd@users.noreply.github.com>
2022-02-14 21:46:37 +0900

    update

--- config/wsl_port.ps1
+++ config/wsl_port.ps1
@@ -2,17 +2,24 @@
 
 # IP アドレス取得
 $IP=(wsl --exec hostname -I | awk '{print $1}')
-$PORT=22000
+$SSH_PORT=22000
+$RDP_PORT=33890
 
 # 古いポートフォワード設定を削除
-netsh interface portproxy delete v4tov4 listenport=$PORT
+netsh interface portproxy delete v4tov4 listenport=$SSH_PORT
+netsh interface portproxy delete v4tov4 listenport=$RDP_PORT
+
 # ポートフォワード設定
-netsh interface portproxy add v4tov4 listenport=$PORT connectaddress=$IP connectport=2222
+netsh interface portproxy add v4tov4 listenport=$SSH_PORT connectaddress=$IP connectport=2222
+netsh interface portproxy add v4tov4 listenport=$RDP_PORT connectaddress=$IP connectport=3390
 
 # 確認コマンド
 # netsh interface portproxy show all
 
 # ポート開放
 if (!(Get-NetFirewallRule -Name "WSL-SSH" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-    New-NetFirewallRule -Name 'WSL-SSH' -DisplayName 'sshd in WSL' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort $PORT
+    New-NetFirewallRule -Name 'WSL-SSH' -DisplayName 'sshd in WSL' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort $SSH_PORT
+}
+if (!(Get-NetFirewallRule -Name "WSL-RDP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    New-NetFirewallRule -Name 'WSL-RDP' -DisplayName 'xrdp in WSL' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort $RDP_PORT
 }
