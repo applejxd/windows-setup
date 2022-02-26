77172d83349a8327e52fe8f7ac1b1c74bfc9f79c
applejxd <9639592+applejxd@users.noreply.github.com>
2022-02-26 09:11:38 +0900

    remove redundant lines

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,5 @@
 # Memo: Japanese comments cause new line error
 
-# for special folders
-$wsh = New-Object -ComObject WScript.Shell
-
 #################
 # Symbolic Link #
 #################
@@ -12,17 +9,15 @@ if (-not (Test-Path $install_path)){
   git clone https://github.com/applejxd/windows-setup.git $install_path
 }
 
-# PowerShell
+# PowerShell Config
 # cf. https://qiita.com/smicle/items/0ca4e6ae14ea92000d18
 # cf. https://docs.microsoft.com/ja-jp/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.2 
-
-# Set-ExecutionPolicy RemoteSigned
 if (Test-Path $Profile.CurrentUserAllHosts) {
   Remove-Item $Profile.CurrentUserAllHosts
 }
 cmd /c mklink $Profile.CurrentUserAllHosts $env:UserProfile\src\windows-setup\config\profile.ps1
 
-# Windows Terminal
+# Windows Terminal COnfig
 if (Test-Path $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json) {
   Remove-Item $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json 
 }
