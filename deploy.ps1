b64eec4913bd84d2664b12d37643b457158a543a
りんごくん <9639592+applejxd@users.noreply.github.com>
2022-05-29 00:54:12 +0900

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,23 @@
 # Memo: Japanese comments cause new line error
 
+Function winst {
+    winget install --silent --accept-package-agreements --accept-source-agreements $args
+}
+
+#-----------------#
+# Package Manager #
+#-----------------#
+
+# cf. https://boxstarter.org/Learn/WebLauncher
+
+# Install boxstarter (and chocolatey simultaneously)
+. { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
+# Run script
+Install-BoxstarterPackage -PackageName "https://raw.githubusercontent.com/applejxd/windows-setup/main/installer/box.ps1" -DisableReboots
+
+# 3rd party
+Invoke-Expression ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/applejxd/windows-setup/main/installer/3rd_party.ps1'))
+
 #--------------#
 # Link Configs #
 #--------------#
@@ -7,7 +25,7 @@
 $install_path = "$env:UserProfile\src\windows-setup"
 if (-not (Test-Path $install_path)){
   if (!(Get-Command git -ea SilentlyContinue)) {
-    winget install --silent --accept-package-agreements --accept-source-agreements Git.Git
+    winst Git.Git
     $env:Path="C:\Progra~1\Git\bin;"+$env:Path
   }
   git clone https://github.com/applejxd/windows-setup.git $install_path
@@ -27,22 +45,8 @@ if (!((Get-ItemProperty $Profile.CurrentUserAllHosts).Mode.Substring(5,1) -eq 'l
 cmd /c mklink $Profile.CurrentUserAllHosts $env:UserProfile\src\windows-setup\config\profile.ps1
 
 # Windows Terminal Config
-winget install --silent --accept-package-agreements --accept-source-agreements Microsoft.WindowsTerminal
+winst Microsoft.WindowsTerminal
 if (Test-Path $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json) {
   Remove-Item $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json 
 }
 cmd /c mklink $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json $env:UserProfile\src\windows-setup\config\settings.json
-
-#-----------------#
-# Package Manager #
-#-----------------#
-
-# cf. https://boxstarter.org/Learn/WebLauncher
-
-# Install boxstarter (and chocolatey simultaneously)
-. { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
-# Run script
-Install-BoxstarterPackage -PackageName "https://raw.githubusercontent.com/applejxd/windows-setup/main/installer/box.ps1" -DisableReboots
-
-# 3rd party
-Invoke-Expression ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/applejxd/windows-setup/main/installer/3rd_party.ps1'))
