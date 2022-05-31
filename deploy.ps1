3b847ed95ac1922d4712648418da9c8ed301061b
りんごくん <9639592+applejxd@users.noreply.github.com>
2022-05-31 23:33:25 +0900

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -19,14 +19,28 @@ Install-BoxstarterPackage -PackageName "https://raw.githubusercontent.com/applej
 
 # cf. https://stackoverflow.com/questions/16023238/installing-system-font-with-powershell
 
-# The CLSID of the special folder
-# cf. https://tarma.com/support/im9/using/symbols/functions/csidls.htm
 if (-not ([System.String]::Join(" ",[System.Drawing.FontFamily]::Families)).Contains("Ricty Diminished for Powerline")){
-    Invoke-WebRequest https://github.com/mzyy94/RictyDiminished-for-Powerline/raw/master/powerline-fontpatched/Ricty%20Diminished%20Regular%20for%20Powerline.ttf -OutFile $Home\RictyDiminished-for-Powerline.ttf
-    (New-Object -ComObject Shell.Application).Namespace(0x14).CopyHere("$Home\RictyDiminished-for-Powerline.ttf", 0x10)
-    Remove-Item $Home\RictyDiminished-for-Powerline.ttf
+  $url = "https://github.com/mzyy94/RictyDiminished-for-Powerline/raw/master/powerline-fontpatched/Ricty%20Diminished%20Regular%20for%20Powerline.ttf"
+  Invoke-WebRequest $url -OutFile $Home\RictyDiminished-for-Powerline.ttf
+  
+  # The CLSID of the special folder
+  # cf. https://tarma.com/support/im9/using/symbols/functions/csidls.htm
+  (New-Object -ComObject Shell.Application).Namespace(0x14).CopyHere("$Home\RictyDiminished-for-Powerline.ttf", 0x10)
+  Remove-Item $Home\RictyDiminished-for-Powerline.ttf
 }
 
+if (-not ([System.String]::Join(" ",[System.Drawing.FontFamily]::Families)).Contains("Cica")){
+  $url = "https://github.com/miiton/Cica/releases/download/v5.0.3/Cica_v5.0.3.zip"
+  Invoke-WebRequest $url -OutFile $Home\Cica.zip
+  Expand-Archive -Path $Home\Cica.zip -DestinationPath $HOME\Cica
+  
+  # The CLSID of the special folder
+  # cf. https://tarma.com/support/im9/using/symbols/functions/csidls.htm
+  (New-Object -ComObject Shell.Application).Namespace(0x14).CopyHere("$Home\Cica\Cica-Regular.ttf", 0x10)
+  Remove-Item $Home\Cica.zip
+  Remove-Item -Recurse\Cica
+}
+ 
 #---------#
 # Startup #
 #---------#
