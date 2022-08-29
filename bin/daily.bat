21a2bc01b2311b5278a7308516b814ef2b2d1361
applejxd <9639592+applejxd@users.noreply.github.com>
2022-08-30 08:15:44 +0900

    update

--- bin/daily.bat
+++ bin/daily.bat
@@ -1,7 +1,15 @@
+@echo off
+
 set year=%date:~0,4%
 set month=%date:~5,2%
 set day=%date:~8,2%
 
-set file_name=%year%-%month%-%day%
-copy %USERPROFILE%\src\windows-setup\bin\daily.md %USERPROFILE%\markdown\daily\%file_name%.md
-code %USERPROFILE%\markdown\daily\%file_name%.md
+set template_file=%USERPROFILE%\src\windows-setup\bin\daily.md
+set root_path=%USERPROFILE%\markdown
+set save_path=%root_path%\daily\%year%\%month%
+set file_path=%save_path%\%date:/=-%.md
+
+if not exist %save_path% mkdir %save_path%
+if not exist %file_path% copy %template_file% %file_path%
+
+code %folder_path% %file_path%
