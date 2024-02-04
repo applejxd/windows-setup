da5779f08731c9d6f3aabd2ef6bbf5653ea41e49
applejxd <9639592+applejxd@users.noreply.github.com>
2024-02-04 15:02:55 +0900

    update

--- installer/ps_sshd.ps1
+++ installer/ps_sshd.ps1
@@ -11,10 +11,12 @@ Start-Service sshd
 # OPTIONAL but recommended:
 Set-Service -Name sshd -StartupType 'Automatic'
 
-# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
-if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
-    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-} else {
-    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
-}
+netsh advfirewall firewall add rule name="sshd" dir=in action=allow protocol=TCP localport=22
+
+# # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+# if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+#     Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+#     New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+# } else {
+#     Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+# }
