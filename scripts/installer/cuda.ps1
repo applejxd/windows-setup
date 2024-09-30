1025023e645e178f1a3f402a9bf4a5dfbe57f035
applejxd <9639592+applejxd@users.noreply.github.com>
2024-10-01 00:30:16 +0900

    update

---
+++ scripts/installer/cuda.ps1
@@ -0,0 +1,5 @@
+winget install Microsoft.VisualStudio.2022.BuildTools 
+winget install Microsoft.VCRedist.2015+.x64
+
+winget install Nvidia.GeForceExperience
+winget install Nvidia.CUDA --version 12.1
