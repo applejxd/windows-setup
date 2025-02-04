8ab3b79488c35b36b0273a5ebe4554bf8a148c52
applejxd <9639592+applejxd@users.noreply.github.com>
2025-02-04 23:08:39 +0900

    エディタ設定を更新し、VSCodeが存在する場合はそれを使用するように変更

--- tools/Keyhac/config.py
+++ tools/Keyhac/config.py
@@ -2,6 +2,7 @@
 Keyhac 設定ファイル
 See https://crftwr.github.io/keyhac/doc/ja/#id8
 """
+
 import datetime
 import os
 import sys
@@ -10,7 +11,7 @@ from pathlib import Path
 import pyauto
 from keyhac import *
 
-keyhac_path = Path(os.environ['USERPROFILE']) / "AppData" / "Roaming" / "Keyhac"
+keyhac_path = Path(os.environ["USERPROFILE"]) / "AppData" / "Roaming" / "Keyhac"
 sys.path.append(str(keyhac_path))
 
 from extension import clipboard
@@ -18,17 +19,27 @@ from extension.fakeymacs import config as fakeymacs
 
 
 def editor(path):
-    shellExecute( None, None, "notepad.exe", '"%s"'% path, "" )
+    # VSCode があれば使用
+    editor_path = Path("C:\Program Files\Microsoft VS Code\Code.exe")
+    if not editor_path.exists():
+        # なければメモ帳
+        editor_path = Path("notepad.exe")
+    editor_path = f'\"{str(editor_path)}\"'
+        
+    # see https://crftwr.github.io/keyhac/doc/ja/group__pyauto.html#gaed9cb14419b28016c15906a08d92102f
+    shellExecute( None, editor_path, '"%s"'% path, "" )
+
 
 def configure(keymap):
     # 設定ファイルを開くエディタ
     keymap.editor = editor
-    
+
     # リストウィンドウを白いテーマで
     keymap.setTheme("white")
 
-    # Emacs キーバインド
-    fakeymacs.configure(keymap)
+    # # Emacs キーバインド
+    # fakeymacs.configure(keymap)
 
     # クリップボード拡張
     clipboard.configure(keymap)
+    
