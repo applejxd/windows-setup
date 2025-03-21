c3e219093ed484677f45ba768e63e7e1215c5339
applejxd <9639592+applejxd@users.noreply.github.com>
2025-03-22 08:39:52 +0900

    settings.jsonとnetsh.ps1の修正: WSL用のstartingDirectoryを追加し、ファイアウォールルールの名前を変更

--- scripts/network/netsh.ps1
+++ scripts/network/netsh.ps1
@@ -1,6 +1,6 @@
 # Windows SSH
-netsh advfirewall firewall add rule name="sshd_win" dir=in action=allow protocol=TCP localport=22
+netsh advfirewall firewall add rule name="OpenSSH Server (Windows)" dir=in action=allow protocol=TCP localport=22
 # WSL SSH
-netsh advfirewall firewall add rule name="sshd_wsl" dir=in action=allow protocol=TCP localport=50022
+netsh advfirewall firewall add rule name="OpenSSH Server (WSL)" dir=in action=allow protocol=TCP localport=50022
 # WSL RDP
-netsh advfirewall firewall add rule name="xrdp" dir=in action=allow protocol=TCP localport=53389
+netsh advfirewall firewall add rule name="xrdp (WSL)" dir=in action=allow protocol=TCP localport=53389
