2dd27e7daf4e5dc05a47554a8607ec373f6b4616
applejxd <9639592+applejxd@users.noreply.github.com>
2025-03-26 01:10:19 +0900

    Nvidia GeForceExperienceのインストールをGeForceNowに変更

--- scripts/installer/cuda.ps1
+++ scripts/installer/cuda.ps1
@@ -1,5 +1,5 @@
 winget install Microsoft.VisualStudio.2022.BuildTools 
 winget install Microsoft.VCRedist.2015+.x64
 
-winget install Nvidia.GeForceExperience
+winget install Nvidia.GeForceNow
 winget install Nvidia.CUDA --version 12.1
